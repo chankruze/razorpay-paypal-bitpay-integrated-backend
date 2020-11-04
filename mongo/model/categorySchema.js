c58ce925666e3fda2c182185dd28c0cc2003526b
chankruze <chankruze@gmail.com>
2020-11-05 00:26:13 +0530

    Adds Category Schema
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ mongo/model/categorySchema.js
@@ -0,0 +1,22 @@
+/*
+Author: chankruze (chankruze@geekofia.in)
+Created: Wed Nov 04 2020 20:12:55 GMT+0530 (India Standard Time)
+
+Copyright (c) Geekofia 2020 and beyond
+*/
+
+const mongoose = require("mongoose");
+
+const keySchema = new mongoose.Schema({
+  name: String,
+  category: String,
+  mrp: Number,
+  price: Number,
+  currency: String,
+  description: String,
+  image: String,
+  tag: String,
+  count: Number,
+});
+
+module.exports = mongoose.model("Category", keySchema);
